69eed2bdcb96cac4b6027b2dfedb1e0ca93acdd5
Chris Riebschlager <riebschlager@gmail.com>
2015-04-22 23:30:43 -0500

    indent

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(request, response) {
-  response.send('Welcome to BLVDIA.');
+    response.send('Welcome to BLVDIA.');
 });
 
-io.on('connection', function(socket){
-  socket.on('shutter', function(msg){
-    io.emit('shutter', msg);
-  });
-  socket.on('snap', function(msg){
-    io.emit('snap', msg);
-  });
-  socket.on('complete', function(msg){
-    io.emit('complete', msg);
-  });
+io.on('connection', function(socket) {
+    socket.on('shutter', function(msg) {
+        io.emit('shutter', msg);
+    });
+    socket.on('snap', function(msg) {
+        io.emit('snap', msg);
+    });
+    socket.on('complete', function(msg) {
+        io.emit('complete', msg);
+    });
 });
 
-http.listen(process.env.PORT || 5000, function(){
-  console.log('listening on *:5000');
+http.listen(process.env.PORT || 5000, function() {
+    console.log('listening on *:5000');
 });
